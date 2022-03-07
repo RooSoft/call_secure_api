760f8c7f8f27d96f56d9d1c660cdf6b56cfb9728
marc@roosoft.com <marc@roosoft.com>
2022-03-07 10:27:07 -0500

    code cleanup

--- call_secure_api.exs
+++ call_secure_api.exs
@@ -1,35 +1,41 @@
 defmodule HTTPGetter do
-  def get_headers macaroon_filename do
+  def get_headers(macaroon_filename) do
     [
-      {'MACAROON_HEADER', 'Grpc-Metadata-macaroon: #{read_macaroon macaroon_filename}'}
+      {'MACAROON_HEADER', 'Grpc-Metadata-macaroon: #{read_macaroon(macaroon_filename)}'}
     ]
   end
 
-  def die msg do
-    IO.puts :stderr, msg; exit { :shutdown, 1 }
+  def die(msg) do
+    IO.puts(:stderr, msg)
+    exit({:shutdown, 1})
   end
 
-  def get macaroon_filename, cert_filename, url do
-    case :httpc.request :get, { String.to_charlist(url), get_headers(macaroon_filename) }, get_options(cert_filename), [] do
-      { :ok, { { _v, 200, _m }, _h, body } } -> :erlang.list_to_binary body
-      { :error, e }                          -> die "Error: #{inspect e}"
+  def get(macaroon_filename, cert_filename, url) do
+    headers = get_headers(macaroon_filename)
+    request = {String.to_charlist(url), headers}
+    options = get_options(cert_filename)
+
+    case :httpc.request(:get, request, options, []) do
+      {:ok, {{_v, 200, _m}, _h, body}} -> :erlang.list_to_binary(body)
+      {:error, e} -> die("Error: #{inspect(e)}")
     end
   end
 
-  def main [cert, macaroon, url] do
-    :ssl.start; :inets.start; get(cert, macaroon, url) |> IO.write
+  def main([cert, macaroon, url]) do
+    :ssl.start()
+    :inets.start()
+    get(cert, macaroon, url) |> IO.write()
   end
 
-  def main(_), do: die "Usage: call_umbrel.exs <cert filename> <macaroon filename> <url>"
+  def main(_), do: die("Usage: call_umbrel.exs <cert filename> <macaroon filename> <url>")
 
-  defp read_macaroon macaroon_filename do
-    File.read!(macaroon_filename) |> Base.encode16
+  defp read_macaroon(macaroon_filename) do
+    File.read!(macaroon_filename) |> Base.encode16()
   end
 
-  defp get_options cert_filename do
-    [ ssl: [ cacertfile: cert_filename ] ]
+  defp get_options(cert_filename) do
+    [ssl: [cacertfile: cert_filename]]
   end
 end
 
-HTTPGetter.main System.argv
-
+HTTPGetter.main(System.argv())
